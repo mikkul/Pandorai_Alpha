a56fe98c227c23b878eca49959596a2ad0ddd72e
mikkul <mikkuleszow@gmail.com>
2022-02-06 02:12:24 +0100

    Implement the speed potion

--- Effects/ModifySpeed.cs
+++ Effects/ModifySpeed.cs
@@ -7,21 +7,36 @@ namespace Pandorai.Effects
         public int Amount;
         public int Duration;
 
+        private int TurnCounter;
+
         public override void SetAttribute(string name, string value)
         {
-            if(name == "Amount")
+            if (name == "Amount")
             {
                 Amount = int.Parse(value);
             }
-            else if(name == "Duration")
+            else if (name == "Duration")
             {
                 Duration = int.Parse(value);
             }
         }
 
+        private Creature _usingCreature;
+
         public override void Use(Creature usingCreature)
         {
-            throw new System.NotImplementedException();
+            _usingCreature = usingCreature;
+            usingCreature.Speed += Amount;
+            usingCreature.TurnCame += CheckDuration;
+        }
+
+        private void CheckDuration()
+        {
+            TurnCounter++;
+            if (TurnCounter >= Duration)
+            {
+                _usingCreature.Speed -= Amount;
+            }
         }
     }
 }
