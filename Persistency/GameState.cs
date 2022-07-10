5bc207e92ff5fc5e265214eadf7fd1204a23c0b9
mikkul <mikkuleszow@gmail.com>
2022-07-10 17:46:26 +0200

    Create base empty classes

--- Persistency/GameState.cs
+++ Persistency/GameState.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace Pandorai.Persistency
 {
     class GameState
     {
-        
+        public TileState[,] Tiles { get; set; }
+        public List<CreatureState> Creatures { get; set; }
+        public List<StructureState> Structures { get; set; }
+        public List<ItemState> Items { get; set; }
     }
 }
---
+++ Persistency/StructureState.cs
@@ -0,0 +1,7 @@
+namespace Pandorai.Persistency
+{
+    class StructureState
+    {
+        
+    }
+}
